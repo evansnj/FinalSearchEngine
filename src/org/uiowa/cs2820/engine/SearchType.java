d2b40a7ab5c82a926fcf69a3542630cacafc61a4
kstasko <kstasko@users.noreply.github.com>
2014-12-07 16:14:04 -0600

    Create SearchType.java
    Psuedocode added for greaterThan() method

---
+++ src/org/uiowa/cs2820/engine/SearchType.java
@@ -0,0 +1,16 @@
+package org.uiowa.cs2820.engine;
+
+public class SearchType (
+  
+  public void greterThan( Field n){
+    //ArrayList<Field> master = new ArrayList<Field>()
+    //for(field a in database)
+    
+      //if a.getFieldName().equals(n.getFieldName()
+        //int flag = a.getFieldValue().compareTo(n.getFieldValue())
+        
+        //if flag == 1
+          //master.add(a);
+      
+        
+  }
