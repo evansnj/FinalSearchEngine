836eaa875259aec5d4d357fb833afb59b40ac754
HaxBagoosh <jason@holyarmy.org>
2014-12-10 16:57:53 -0600

    Redid ComboSearch
    Fixed some smelly code. Made it reuse code by putting it in the
    constructor.

--- src/org/uiowa/cs2820/engine/ComboSearch.java
+++ src/org/uiowa/cs2820/engine/ComboSearch.java
@@ -3,20 +3,20 @@ package org.uiowa.cs2820.engine;
 import java.util.*;
 
 public class ComboSearch {
-	Field targeta;
-	Field targetb;
-	Database D;
+	String [] R;
+	String [] S;
+	ArrayList<String> T;
 	public ComboSearch(Field f, Field g) { 
-		D = new LinearDiskDatabase();
-		targeta = f;
-		targetb = g;
-		}
-	public String[] findAnd() {
-		ArrayList<String> T = new ArrayList<String>();
-		String [] R = D.fetch(targeta);
+		Database D = new LinearDiskDatabase();
+		Field targeta = f;
+		Field targetb = g;
+		T = new ArrayList<String>();
+		R = D.fetch(targeta);
 		if (R == null) R = new String[0];
-		String [] S = D.fetch(targetb);
-		if (S == null) S = new String[0];		
+		S = D.fetch(targetb);
+		if (S == null) S = new String[0];
+	}
+	public String[] findAnd() {	
 		for (String i : R){
 			for (String k : S){
 				if (i.equals(k)) T.add(i);
@@ -26,18 +26,13 @@ public class ComboSearch {
 		return Andcontent;
 		}
 	public String[] findOr() {
-		ArrayList<String> X = new ArrayList<String>();
-		String [] U = D.fetch(targeta);
-		if (U == null) U = new String[0];
-		String [] V = D.fetch(targetb);
-		if (V == null) V = new String[0];
-		for (String i : U){
-			X.add(i);
+		for (String i : R){
+			T.add(i);
 		}
-		for (String k : V){
-			X.add(k);
+		for (String k : S){
+			T.add(k);
 			}
-		ArrayList<String> removeduplicates = new ArrayList<String>(new LinkedHashSet<String>(X));
+		ArrayList<String> removeduplicates = new ArrayList<String>(new LinkedHashSet<String>(T));
 		String[] Orcontent = new String[removeduplicates.size()];
 		return Orcontent;
 		}
