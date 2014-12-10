4e26256ef5414b478e04e480bce577c080918481
kstasko <kstasko@users.noreply.github.com>
2014-12-10 15:46:24 -0600

    Update SearchType.java
    updated greaterThan syntax. still trouble with handling integers

--- src/org/uiowa/cs2820/engine/SearchType.java
+++ src/org/uiowa/cs2820/engine/SearchType.java
@@ -1,17 +1,24 @@
 package org.uiowa.cs2820.engine;
 
+import java.util.Arraylist;
+
 public class SearchType (
   
+public class SearchType {
+  
   public void greaterThan( Field n){
-    //ArrayList<Field> master = new ArrayList<Field>()
-    //for(field a in database)
+    ArrayList<Field> master = new ArrayList<Field>();
+    for(Field a : database){
     
-      //if a.getFieldName().equals(n.getFieldName()
-        //int flag = a.getFieldValue().compareTo(n.getFieldValue())
+      if (a.getFieldName().equals(n.getFieldName())){
+        int flag = a.getFieldValue().compareTo(n.getFieldValue());
         
-        //if flag == 1
-          //master.add(a);
-    //return master
+        if (flag == 1){
+          master.add(a);
+        }
+      }
+    }
+    return master;
   }
   
   public void lessThan( Field n){
