eff4838e115d80a96bd76d87c1c05dec4d1b9364
HaxBagoosh <jason@holyarmy.org>
2014-12-10 14:34:21 -0600

    And and Or about done

--- src/org/uiowa/cs2820/engine/ComboSearch.java
+++ src/org/uiowa/cs2820/engine/ComboSearch.java
@@ -1,5 +1,7 @@
 package org.uiowa.cs2820.engine;
 
+import java.util.*;
+
 public class ComboSearch {
 	Field targeta;
 	Field targetb;
@@ -10,19 +12,33 @@ public class ComboSearch {
 		targetb = g;
 		}
 	public String[] findAnd() {
+		ArrayList<String> T = new ArrayList<String>();
 		String [] R = D.fetch(targeta);
 		if (R == null) R = new String[0];
 		String [] S = D.fetch(targetb);
-		if (S == null) S = new String[0];
-		
-		return T;
+		if (S == null) S = new String[0];		
+		for (String i : R){
+			for (String k : S){
+				if (i.equals(k)) T.add(i);
+			}
+		}
+		String[] Andcontent = new String[T.size()];
+		return Andcontent;
 		}
 	public String[] findOr() {
+		ArrayList<String> X = new ArrayList<String>();
 		String [] U = D.fetch(targeta);
 		if (U == null) U = new String[0];
 		String [] V = D.fetch(targetb);
 		if (V == null) V = new String[0];
-		
-		return X;
+		for (String i : U){
+			X.add(i);
+		}
+		for (String k : V){
+			X.add(k);
+			}
+		ArrayList<String> removeduplicates = new ArrayList<String>(new LinkedHashSet<String>(X));
+		String[] Orcontent = new String[removeduplicates.size()];
+		return Orcontent;
 		}
 	}
