422d6e7610c6a489fc9b45f6c823973a3c60461f
kstasko <kstasko@users.noreply.github.com>
2014-12-10 16:03:59 -0600

    Update SearchType.java
    updated all methods to 'functional'. eclipse still flags error for Field a.getFieldName() and cannot import java.uitl.ArrayList. Looking into it.

--- src/org/uiowa/cs2820/engine/SearchType.java
+++ src/org/uiowa/cs2820/engine/SearchType.java
@@ -1,19 +1,16 @@
-package org.uiowa.cs2820.engine;
+package src.org.uiowa.cs2820.engine;
 
-import java.util.Arraylist;
+import java.util.ArrayList;
 
-public class SearchType (
-  
 public class SearchType {
   
-  public void greaterThan( Field n){
+  public void greaterThan(Field n){
     ArrayList<Field> master = new ArrayList<Field>();
-    for(Field a : database){
+    for(Field a : database){   	
     
-      if (a.getFieldName().equals(n.getFieldName())){
-        int flag = a.getFieldValue().compareTo(n.getFieldValue());
+    	if (a.getFieldName().equals(n.getFieldName())){
         
-        if (flag == 1){
+        if (a.getFieldName().compareTo(n.getFieldName() == 1)){
           master.add(a);
         }
       }
@@ -22,35 +19,42 @@ public class SearchType {
   }
   
   public void lessThan( Field n){
-    //ArrayList<Field> master = new ArrayList<Field>()
-    //for(field a in database)
-    
-      //if a.getFieldName().equals(n.getFieldName()
-        //int flag = a.getFieldValue().compareTo(n.getFieldValue())
-        
-        //if flag == -1
-          //master.add(a)
-    //return master
-  }
+	  ArrayList<Field> master = new ArrayList<Field>();
+	    for(Field a : database){   	
+	    
+	      if (a.getFieldName().equals(n.getFieldName())){
+	        
+	        if (a.getFieldName().compareTo(n.getFieldName() == -1)){
+	          master.add(a);
+	        }
+	      }
+	    }
+	    return master;
+	  }
     public void equals( Field n){
-      //ArrayList<Field> master = new ArrayList<Field>()
-      //for(field a in database)
-    
-      //if a.getFieldName().equals(n.getFieldName()
-        //int flag = a.getFieldValue().compareTo(n.getFieldValue())
-        
-        //if flag == 0
-          //master.add(a)
-    //return master
-    }
+  	  ArrayList<Field> master = new ArrayList<Field>();
+	    for(Field a : database){   	
+	    
+	      if (a.getFieldName().equals(n.getFieldName())){
+	        
+	        if (a.getFieldName().compareTo(n.getFieldName() == 0)){
+	          master.add(a);
+	        }
+	      }
+	    }
+	    return master;
+	  }
       public void prefix( Field n){
-        //ArrayList<Field> master = new ArrayList<Field>()
-        //for(field a in database)
+       ArrayList<Field> master = new ArrayList<Field>()
+       for(Field a : database){
     
-       //if a.getFieldName().equals(n.getFieldName())
+       if (a.getFieldName().equals(n.getFieldName())){
        
-        //if a.getFieldValue().substring(0, n.getFieldValue().length()) == n.getFieldValue()
-            //master.add(a);
-            
-      //return master
+       	if (a.getFieldValue().substring(0, n.getFieldValue().length()) == n.getFieldValue()){
+            master.add(a);
+       			}
+       		}
+       }
+      return master;
       }
+}
