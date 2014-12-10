66239eaeaabc9f90e29bbf21d71c828ec5a62ffe
kstasko <kstasko@users.noreply.github.com>
2014-12-10 16:20:34 -0600

    Update SearchType.java
    import errors are in regard to source issue. Will discuss with group in meeting.

--- src/org/uiowa/cs2820/engine/SearchType.java
+++ src/org/uiowa/cs2820/engine/SearchType.java
@@ -12,46 +12,47 @@ public class SearchType {
         
         if (a.getFieldName().compareTo(n.getFieldName() == 1)){
           master.add(a);
-        }
-      }
+        	}
+    	}
     }
     return master;
   }
   
   public void lessThan( Field n){
 	  ArrayList<Field> master = new ArrayList<Field>();
-	    for(Field a : database){   	
+	  for(Field a : database){   	
 	    
-	      if (a.getFieldName().equals(n.getFieldName())){
+		  if (a.getFieldName().equals(n.getFieldName())){
 	        
-	        if (a.getFieldName().compareTo(n.getFieldName() == -1)){
-	          master.add(a);
-	        }
-	      }
-	    }
-	    return master;
+	      if (a.getFieldName().compareTo(n.getFieldName() == -1)){
+	    	  master.add(a);
+	      	  }
+		  }
 	  }
-    public void equals( Field n){
-  	  ArrayList<Field> master = new ArrayList<Field>();
-	    for(Field a : database){   	
+	  return master;
+  }
+  
+  public void equals( Field n){
+	  ArrayList<Field> master = new ArrayList<Field>();
+	  for(Field a : database){   	
 	    
-	      if (a.getFieldName().equals(n.getFieldName())){
+		  if (a.getFieldName().equals(n.getFieldName())){
 	        
-	        if (a.getFieldName().compareTo(n.getFieldName() == 0)){
-	          master.add(a);
-	        }
+	      if (a.getFieldName().compareTo(n.getFieldName() == 0)){
+	    	  master.add(a);
+	          }
 	      }
-	    }
-	    return master;
 	  }
-      public void prefix( Field n){
-       ArrayList<Field> master = new ArrayList<Field>()
-       for(Field a : database){
+	  return master;
+	  }
+  public void prefix( Field n){
+	  ArrayList<Field> master = new ArrayList<Field>()
+      for(Field a : database){
     
-       if (a.getFieldName().equals(n.getFieldName())){
+    	  if (a.getFieldName().equals(n.getFieldName())){
        
-       	if (a.getFieldValue().substring(0, n.getFieldValue().length()) == n.getFieldValue()){
-            master.add(a);
+    		  if (a.getFieldValue().substring(0, n.getFieldValue().length()) == n.getFieldValue()){
+    			  master.add(a);
        			}
        		}
        }
