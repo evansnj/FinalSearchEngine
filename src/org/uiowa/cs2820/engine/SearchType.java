c4275901d14e8b74e7f8a4efbb5bf4f5ab268ab2
kstasko <kstasko@users.noreply.github.com>
2014-12-11 13:43:13 -0600

    Update SearchType.java

--- src/org/uiowa/cs2820/engine/SearchType.java
+++ src/org/uiowa/cs2820/engine/SearchType.java
@@ -3,8 +3,9 @@ package org.uiowa.cs2820.engine;
 import java.util.ArrayList;
 
 public class SearchType {
+  ArrayList<Field> D;
   
-  public ArrayList<Field>greaterThan(Field n, ArrayList<Field> D){
+  public ArrayList<Field> greaterThan(Field n){
     ArrayList<Field> master = new ArrayList<Field>();
     for(Field a : D){   	
     
@@ -51,7 +52,7 @@ public class SearchType {
     
     	  if (a.getFieldName().equals(n.getFieldName())){
        
-    		  if (a.getFieldValue().substring(0, n.getFieldValue().length()) == n.getFieldValue()){
+    		  if (a.getFieldValue().substring(0, (n.getFieldValue().toString().length())) == n.getFieldValue()){
     			  master.add(a);
        			}
        		}
