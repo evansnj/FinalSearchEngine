315bcc335ae54ed7dfd150bd493efc82c0f04e95
Brandon Sosa <brandon-sosa@uiowa.edu>
2014-12-11 14:14:31 -0600

    Iterator initial code

---
+++ src/org/uiowa/cs2820/engine/Iterator.java
@@ -0,0 +1,11 @@
+Search<String> stack = new ReturnedSearch<String>();
+        i = stack.iterator();
+        while (i.hasNext()){
+        String element = i.next()
+        // process element
+        }
+
+        for (String element : stack){
+        // process element
+
+        }
