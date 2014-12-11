931af150405e6bb973fd84f64608afe738e99b7a
Nick Evans <nicholas-evans@uiowa.edu>
2014-12-11 10:28:33 -0600

    Add resource for indexing

--- src/org/uiowa/cs2820/engine/Server.java
+++ src/org/uiowa/cs2820/engine/Server.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -30,30 +31,51 @@ public class Server {
     	return "Hello, Jersey!\n";
     }
     
-    // Should this return a JSON array of results? Or some structure fit for a template?
+    // Current state: only uses the first two search terms
     @GET
     @Path("search")
     @Produces(MediaType.TEXT_PLAIN) //TODO: change to actually useful type
     public Response search(@QueryParam("q") String q) {
 //    	System.out.println(q);
-    	//Make the whitespace-separated query into a list
-    	List<String> ql = Arrays.asList(q.split("\\s+"));
-//    	for (String elem : ql) {
+    	// Extract search terms from querystring
+    	List<String> params = Arrays.asList(q.split("\\s+"));
+//    	for (String elem : params) {
 //    		System.out.println(elem);
 //    	}
-    	Field f = new Field(ql.get(0), ql.get(1)); //TODO: limit q terms to 2
+    	Field f = new Field(params.get(0), params.get(1)); //TODO: limit q terms to 2
     	FieldSearch fs = new FieldSearch(f);
     	String[] s = fs.findEquals();
     	String results = Arrays.toString(s);
     	System.out.println(results);
     	return Response //TODO: change to a useful response
     			.status(200)
-    			.entity("search query: " + ql.toString() +
+    			.entity("search query: " + params.toString() +
     					"\n     results: " + results).build();
     //URL: http://localhost:8080/search?q=not%20riparia
     //response: "search query: [not riparia]
     //                results: [1.txt, 4.txt, 0.txt]"
+    }
     
+    // What should be allowed to be indexed?
+    // Using a two-string field for now, similar to searching
+    @PUT
+    @Path("index")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void index(
+    		@FormParam("id") String id,
+    		@FormParam("field") String fields) {
+    	// Extract index text from querystring
+    	List<String> params = Arrays.asList(fields.split("\\s+"));
+    	System.out.println(id);
+    	System.out.println(params.toString());
+    	Field f = new Field(params.get(0), params.get(1)); //TODO: limit q terms to 2
+    	Indexer I = new Indexer(id);
+    	I.addField(f);
+    	FieldSearch fs = new FieldSearch(f);
+    	Boolean b = fs.findEquals().length > 0;
+    	if (b == true) {
+    		System.out.println("Object added to database");
+    	}
     }
 }
 
