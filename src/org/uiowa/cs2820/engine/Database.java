c5c0c5926c979cb08b028e17e50c146b4bc2bf52
kstasko <kstasko@users.noreply.github.com>
2014-12-12 12:36:28 -0600

    Update Database.java

--- src/org/uiowa/cs2820/engine/Database.java
+++ src/org/uiowa/cs2820/engine/Database.java
@@ -1,6 +1,7 @@
 package org.uiowa.cs2820.engine;
 
 public interface Database {
+	  public abstract String[] prefix(Field f);
 	  public abstract String[] fetchLS(Field f); //fetch a Node that is small
 	  public abstract String[] fetchGT(Field f);
 	  public abstract String[] fetch(Field f);  // fetch a Node by Field 
