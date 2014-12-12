771a6ae99fe8b9f25241f18a5f2231446c1131d1
kstasko <kstasko@users.noreply.github.com>
2014-12-12 12:15:08 -0600

    Update Database.java

--- src/org/uiowa/cs2820/engine/Database.java
+++ src/org/uiowa/cs2820/engine/Database.java
@@ -1,6 +1,8 @@
 package org.uiowa.cs2820.engine;
 
 public interface Database {
+	  public abstract String[] fetchLS(Field f); //fetch a Node that is small
+	  public abstract String[] fetchGT(Field f);
 	  public abstract String[] fetch(Field f);  // fetch a Node by Field 
 	  public abstract void store(Field f, String id); // store a Field/id
 	  public abstract void remove(String id); // delete document (by identifier) 
