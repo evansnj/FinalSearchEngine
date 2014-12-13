71e325e6388d676cf23c3b07328efd07caa96dea
kstasko <kstasko@users.noreply.github.com>
2014-12-12 18:21:39 -0600

    Update SearchTypeTest.java
    Added more tests

--- src/org/uiowa/cs2820/engine/SearchTypeTest.java
+++ src/org/uiowa/cs2820/engine/SearchTypeTest.java
@@ -14,6 +14,7 @@ class SearchTypeTest {
   D.store(new Field("Name", "Deborah"),"Deborah");
   D.store(new Field("Name", "Jim"), "Jim");
   D.store(new Field("Part", "Axel"), "Axel");
+  D.store(new Field("Name", "Jimmy"), "Jimmy");
   D.store(new Field("Animal", "Zebra"),"Zebra");
   D.store(new Field("Animal", "Dog"), "Dog");
   D.store(new Field("Part", "Wheel"), "Wheel");
@@ -23,11 +24,29 @@ class SearchTypeTest {
   //...and so on
   
   @Test
-  Field f = new Field("Animal", "Dog");
-  FieldSearch F = new FieldSearch.findLS(f);
-  String [] S = F.findLS();
-  assertEquals(S.length,1);
+  public void Test1(){
+    Field f = new Field("Animal", "Dog");
+    FieldSearch F = new FieldSearch.findLS(f);
+    String [] S = F.findLS();
+    assertEquals(S.length,1);
+  }
   
+  @Test
+  public void Test2(){
+    Field g = new Field("Serial", 12345);
+    FieldSearch F = new FieldSearch.findGT(f);
+    String [] S = F.findGT();
+    assertEquals(S.length,1);
+
+  }
+  
+  @Test 
+  public void Test3(){
+    Field g = new Field("Name", "Ji");
+    FieldSearch F = new FieldSearch.findPrefix(g);
+    String [] S = F.findPrefix();
+    assertEquals(S.length, 2);
+  }
   
   
   
